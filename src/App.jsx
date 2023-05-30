0b202985d484809eebb632a101f00fe66fa4c612
Bolexzy <boluoyewumi79@gmail.com>
2023-05-30 02:42:19 +0100

    Add feature: improve content and responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import Info from "./components/Info";
 import About from "./components/About";
 import Interests from "./components/Interests";
+import Skills from "./components/Skills";
 import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="card--container">
       <Info />
-      <About />
-      <Interests />
+      <div className="card--content">
+        <About />
+        <Interests />
+        <Skills />
+      </div>
       <Footer />
     </div>
   );
