50518141a14520dd7a12d81781bcc3678ace3b7a
Bolexzy <boluoyewumi79@gmail.com>
2023-05-30 02:41:05 +0100

    Add feature: improve content and responsiveness

---
+++ src/components/Skills.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div class="skills">
+      <h2>Skills</h2>
+      <ul class="skills-buttons">
+        <li>Project Manager</li>
+        <li>Digital Marketer</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JS</li>
+        <li>Python</li>
+        <li>React</li>
+        <li>C</li>
+        <li>Flask</li>
+        <li>Ruby</li>
+        <li>....</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Skills;
